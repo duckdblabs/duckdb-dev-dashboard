1b139c17a1f5f602d253ef14eb085ea0e561db81
Christiaan Herrewijn <christiaan@duckdblabs.com>
2025-11-19 18:10:15 +0100

    revert materialized view logic

--- evidence/sources/generate_sources.py
+++ evidence/sources/generate_sources.py
@@ -14,7 +14,6 @@ from utils.ducklake import DuckLakeConnection
 def main():
     # get config (e.g. which tables apply for which source)
     sources_config = Path("./evidence/sources/sources.json")
-    sources_dir = Path("./evidence/sources")
     if not sources_config.is_file():
         print(f"Error: source config file not found at {sources_config}")
         sys.exit(1)
@@ -22,7 +21,6 @@ def main():
 
     # create .duckdb source files from ducklake
     with DuckLakeConnection() as con:
-        # create base tables
         for source in sources:
             print(f"---\ngenerating sources for data-feed: {source['name']} ...")
             con.execute(f"ATTACH '{source['db_path']}' AS {source['name']};")
@@ -32,18 +30,6 @@ def main():
                     print(f"Refreshed file '{source['db_path']}', table: {table} by copying from ducklake", flush=True)
                 else:
                     print(f"Error: table {table} not present in ducklake; can not refresh: {source['db_path']}!")
-
-            if "materialized_views" in source:
-                # Materialized views (implemented as regular tables)
-                for mview in source["materialized_views"]:
-                    query_file: Path = sources_dir / mview["query_file"]
-                    if query_file.is_file():
-                        mview_name = mview["table_name"]
-                        mv_query = query_file.read_text()
-                        con.execute(f"CREATE OR REPLACE TABLE {source['name']}.main.{mview_name} AS {mv_query};")
-                        print(f"Refreshed mview: '{mview_name}' by running query file: {query_file.name}", flush=True)
-                    else:
-                        print(f"Error: can not re-create MView: {mview_name}; file {query_file} not found!")
 
 
 if __name__ == "__main__":
