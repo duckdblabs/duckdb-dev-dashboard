606e5023e4448497b40fc07adf4bb3b92bca1c7b
Christiaan Herrewijn <christiaan@ducklabs.com>
2026-06-08 13:31:05 +0200

    smoke test for github action maintain-ducklake

--- utils/maintainance.sql
+++ utils/maintainance.sql
@@ -1,5 +1,7 @@
-ATTACH 'ducklake:ducklake_secret' AS my_ducklake (AUTOMATIC_MIGRATION);
+ATTACH 'ducklake:ducklake_secret' AS my_ducklake;
 USE my_ducklake;
+show tables;
+select updated_at from ci_repositories where name = 'duckdb';
 
-CALL ducklake_merge_adjacent_files('my_ducklake');
-CALL ducklake_cleanup_old_files('my_ducklake', cleanup_all => true);
+-- CALL ducklake_merge_adjacent_files('my_ducklake');
+-- CALL ducklake_cleanup_old_files('my_ducklake', cleanup_all => true);
